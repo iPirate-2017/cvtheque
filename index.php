6aa4cf2dc01215c655b0b1a8e07730e2c93b801b
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-06-22 15:39:58 +0400

    Add index to commit

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-	$colors = ["#1abc9c", "#e67e22", "#8e44ad", "#f39c12", "#2ecc71", "#34495e"];
+	$colors = [
+		"#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#2ecc71", "#16a085", "#27ae60",
+		"#2980b9", "#8e44ad", "#f1c40f", "#e67e22", "#e74c3c", "#f39c12", "#d35400",
+		"#c0392b"
+];
 	$dir = "cv/custom/";
 	$cvs = [];
 	if (is_dir($dir)) {
@@ -17,7 +21,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Multiverse by HTML5 UP</title>
+		<title>CVthèque - IPI Réunion</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -33,11 +37,11 @@
 				<!-- Header -->
 					<header id="header">
 						<h1><strong>CVthèque</strong> IPI Réunion</h1>
-						<!-- <nav>
+						<nav>
 							<ul>
-								<li><a href="#footer" class="icon fa-info-circle">About</a></li>
+								<li><a href="#footer" class="button" style="margin-right: 20px">Thèmes</a></li>
 							</ul>
-						</nav> -->
+						</nav>
 					</header>
 
 				<!-- Main -->
@@ -51,7 +55,7 @@
 				</div>
 
 				<!-- Footer -->
-					<footer id="footer" class="panel">
+					<!-- <footer id="footer" class="panel">
 						<div class="inner split">
 							<div>
 								<section>
@@ -74,7 +78,7 @@
 								</p>
 							</div>
 						</div>
-					</footer>
+					</footer> -->
 
 			</div>
 
